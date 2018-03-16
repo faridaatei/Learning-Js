2cce599470165745e9a35fb6fb4fc992bddd03d8
Farida Atei <faridaabdalla0@gmail.com>
2018-03-16 17:09:51 +0300

    added a repo

---
+++ FunctionParameter.js
@@ -0,0 +1,14 @@
+function area_triangle( base,height){
+var area_triangle = 0.5*base*height
+	return area_triangle
+}
+var answer=area_triangle(9,10);
+var answer2=area_triangle(12,9);
+var anser3+area_triangle(15,7);
+print(answer)
+print(answer two)
+print(answer three)
+
+function print(answer){
+  console.log(area of area_triangle:"answer)
+}
---
+++ Functions.js
@@ -0,0 +1,36 @@
+//void function
+function add(){
+	var numberone =10;
+	var numberTwo=20;
+	var sum =numberone +numberTwo;
+	console.log("sum")
+
+	}
+	//call the function
+	 add()
+
+//create a function called multiply
+	 function multiply(){
+	 	var numberone=1;
+	 	var numbertwo =2;
+	 	var numberthree=3;
+	
+	 var number=numberone*numberthree*numbertwo;
+      return number
+	 }
+
+	//call the function
+	var answer = multiply()
+
+	 console.log(answer)
+
+
+	 function divide (){
+	 	var numberone=30;
+	 	var numbertwo=15;
+	 	var answer=numberone/numbertwo;
+	 	return answer
+
+	 } 
+	 var x=divide()
+	 console.log(x)
